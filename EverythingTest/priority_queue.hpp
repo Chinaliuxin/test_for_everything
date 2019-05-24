6233afbc03a3026665d7dcb6abf1f2f7692730c3
Chinaliuxin <1364635955@qq.com>
2019-05-24 16:38:05 +0800

    heap的运用

--- EverythingTest/priority_queue.hpp
+++ EverythingTest/priority_queue.hpp
@@ -6,6 +6,28 @@
 
 using namespace std;
 namespace mydeque {
+
+
+	class Solution {
+		/*如何得到一个数据流中的中位数？如果从数据流中读出奇数个数值，
+		那么中位数就是所有数值排序之后位于中间的数值。如果从数据流中读出偶数个数值，
+		那么中位数就是所有数值排序之后中间两个数的平均值。我们使用Insert()方法读取数据流，
+		使用GetMedian()方法获取当前读取数据的中位数。
+		*/
+		priority_queue<int, vector<int>, std::less<int> > p;
+		priority_queue<int, vector<int>, greater<int> > q;
+
+	public:
+		void Insert(int num) {
+			if (p.empty() || num <= p.top()) p.push(num);
+			else q.push(num);
+			if (p.size() == q.size() + 2) q.push(p.top()), p.pop();
+			if (p.size() + 1 == q.size()) p.push(q.top()), q.pop();
+		}
+		double GetMedian() {
+			return p.size() == q.size() ? (p.top() + q.top()) / 2.0 : p.top();
+		}
+	};
 	template<class T, class Con = ::std::deque<T>>
 	class Stack
 	{
