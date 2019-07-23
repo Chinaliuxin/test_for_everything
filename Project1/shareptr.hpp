86667db67e530759cbf3663a5e265edf950f8c93
Chinaliuxin <1364635955@qq.com>
2019-07-23 23:29:24 +0800

    ptr

--- Project1/shareptr.hpp
+++ Project1/shareptr.hpp
@@ -109,3 +109,27 @@ int testptr()
 	return 0;
 }
 
+
+template<class T>
+class UniquePtr
+{
+public:
+	UniquePtr(T * ptr = nullptr)
+		: _ptr(ptr)
+	{}
+	~UniquePtr()
+	{
+		if (_ptr)
+			delete _ptr;
+	}
+	T& operator*() { return *_ptr; }
+	T* operator->() { return _ptr; }
+private:
+	// C++98防拷贝的方式：只声明不实现+声明成私有
+	UniquePtr(UniquePtr<T> const &);
+	UniquePtr & operator=(UniquePtr<T> const &);
+	// C++11防拷贝的方式：delete
+	UniquePtr(UniquePtr<T> const &) = delete;
+	UniquePtr & operator=(UniquePtr<T> const &) = delete;
+private: T * _ptr;
+};
