31a9ba203ee2fcaf74916a7feef806b0f0c42040
Chinaliuxin <1364635955@qq.com>
2019-07-28 19:40:40 +0800

    change

--- Project1/求坏球.hpp
+++ Project1/求坏球.hpp
@@ -53,3 +53,94 @@ public:
 			return 0;
 	}
 };
+
+//NowCoder最喜欢游乐场的迷宫游戏，他和小伙伴们比赛谁先走出迷宫。
+//现在把迷宫的地图给你，你能帮他算出最快走出迷宫需要多少步吗？
+
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<functional>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <exception>
+#include <iomanip>
+#include <memory>
+#include <sstream>
+
+using namespace std;
+
+bool check(int x, int y, int m, int n, vector<vector<char>>& room)
+{
+	return x >= 0 && x < m && y >= 0 && y < n && room[x][y] == '.';
+}
+
+int main_zoumigong(int argc, char** argv)
+{
+	//freopen("in.txt", "r", stdin);
+	int m = 10, n = 10;
+	string row;
+	while (cin >> row)
+	{
+		vector<vector<char>> room(m, vector<char>(n));
+		for (int i = 0; i < m; ++i)
+		{
+			if (i > 0) cin >> row;
+			for (int j = 0; j < n; ++j)
+			{
+				room[i][j] = row[j];
+			}
+		}
+
+		int sx = 0, sy = 1;
+		int ex = 9, ey = 8;
+		vector<pair<int, int>> cur;
+		cur.emplace_back(sx, sy);
+		room[sx][sy] = ' ';
+		int step = 0;
+		bool reach = false;
+		while (!cur.empty())
+		{
+			vector<pair<int, int>> next;
+			for (auto& pr : cur)
+			{
+				if (pr.first == ex && pr.second == ey)
+				{
+					reach = true;
+					break;
+				}
+				int x = pr.first, y = pr.second;
+				if (check(x - 1, y, m, n, room))
+				{
+					room[x - 1][y] = ' ';
+					next.emplace_back(x - 1, y);
+				}
+				if (check(x + 1, y, m, n, room))
+				{
+					room[x + 1][y] = ' ';
+					next.emplace_back(x + 1, y);
+				}
+				if (check(x, y - 1, m, n, room))
+				{
+					room[x][y - 1] = ' ';
+					next.emplace_back(x, y - 1);
+				}
+				if (check(x, y + 1, m, n, room))
+				{
+					room[x][y + 1] = ' ';
+					next.emplace_back(x, y + 1);
+				}
+			}
+			if (reach) break;
+			++step;
+			cur = next;
+		}
+
+		cout << step << endl;
+	}
+
+	return 0;
+}
