19ced529ef39586680decce74cd1303a3825ba1e
Chinaliuxin <1364635955@qq.com>
2019-08-01 23:48:59 +0800

    后序

--- Project1/tree.hpp
+++ Project1/tree.hpp
@@ -64,3 +64,38 @@ public:
 		return ret;
 	}
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ 给定一个二叉树，返回它的 后序 遍历。
+ https://leetcode-cn.com/problems/binary-tree-postorder-traversal/
+ */
+class Solution {
+public:
+	vector<int> postorderTraversal(TreeNode* root) {
+		vector<int> ret(0, 0);
+		if (root == nullptr)return ret;
+		stack<TreeNode*> s;
+		s.push(root);
+		TreeNode* cur = nullptr;
+		while (!s.empty())
+		{
+			cur = s.top();
+			ret.push_back(cur->val);
+			s.pop();
+			if (cur->left != nullptr)
+				s.push(cur->left);
+			if (cur->right != nullptr)
+				s.push(cur->right);
+		}
+		reverse(ret.begin(), ret.end());
+		return ret;
+	}
+
+};
