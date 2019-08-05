2eb193857a68eebc13c905c77e62368ffea95fab
Chinaliuxin <1364635955@qq.com>
2019-08-05 22:09:13 +0800

    change

--- Project1/tree.hpp
+++ Project1/tree.hpp
@@ -161,4 +161,29 @@ public:
 
 	}
 };
-
+/*
+https://leetcode-cn.com/problems/unique-paths/
+leetcode 64 ²»Í¬Â·¾¶
+*/
+class Solution {
+public:
+	int uniquePaths(int m, int n) {
+		vector<vector<int> > map(m, vector<int>(n, 0));
+		for (int i = 0; i < map.size(); i++)
+		{
+			map[i][0] = 1;
+		}
+		for (int i = 0; i < map[0].size(); i++)
+		{
+			map[0][i] = 1;
+		}
+		for (int i = 1; i < map.size(); i++)
+		{
+			for (int j = 1; j < map[0].size(); j++)
+			{
+				map[i][j] = map[i - 1][j] + map[i][j - 1];
+			}
+		}
+		return map[m - 1][n - 1];
+	}
+};
